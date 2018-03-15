c25e60aed97a69839d101c6282a42a7fb401f07f
Christopher Banh <chris@chrisbanh.com>
2018-03-14 23:57:51 -0700

    Adds player invulnerability and flashing when hit

--- Game3/Assets/Scripts/Player.cs
+++ Game3/Assets/Scripts/Player.cs
@@ -12,8 +12,17 @@ public class Player : MonoBehaviour {
 	public Text playerHealthText;
 	public GameObject bulletPrefab;
 	public GameObject bulletSpawn;
+	public int invulnerableCooldown;
 
+	private GameObject sprite;
     private int level;
+
+    private float timer;
+    private float flashCooldown;
+    private float invulnerableEnd;
+
+    private bool isInvulnerable;
+    private bool isSpriteShowing;
 
     void Start () {
 		if (SceneManager.GetActiveScene ().buildIndex == 1) {
@@ -22,6 +31,12 @@ public class Player : MonoBehaviour {
 			setCountText ();
 			setHealthText ();
 		}
+		sprite = transform.Find("Sprite").gameObject;
+		timer = 0;
+		flashCooldown = 0;
+		invulnerableEnd = 0;
+		isInvulnerable = false;
+		isSpriteShowing = true;
 	}
 
 	// Update is called once per frame
@@ -29,20 +44,45 @@ public class Player : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.V)) {
 			Shoot ();
 		}
+
+		if (isInvulnerable) {
+			if (timer >= flashCooldown) {
+				flashCooldown = timer + 0.1f;
+				isSpriteShowing = !isSpriteShowing;
+				sprite.GetComponent<Renderer>().enabled = isSpriteShowing;
+			}
+
+			if (timer > invulnerableEnd) {
+				isInvulnerable = false;
+				isSpriteShowing = true;
+				sprite.GetComponent<Renderer>().enabled = true;
+			}
+		}
+
+		timer += Time.deltaTime;
+	}
+
+	void hurtPlayer(int dmg) {
+		if (!isInvulnerable) {
+			FindObjectOfType<SoundEffectManager>().Play ("PlayerDmg");
+			transform.parent.gameObject.GetComponent<PlayerHealth>().subtractPlayerHealth(dmg);
+
+			isInvulnerable = true;
+			flashCooldown = timer;
+			invulnerableEnd = timer + invulnerableCooldown;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
 		if (other.gameObject.CompareTag ("Zombie")) {
-			FindObjectOfType<SoundEffectManager>().Play ("PlayerDmg");
-			transform.parent.gameObject.GetComponent<PlayerHealth>().subtractPlayerHealth(10);
+			hurtPlayer(10);
 			// setHealthText ();
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag ("ZombBullet")) {
-			FindObjectOfType<SoundEffectManager>().Play ("PlayerDmg");
-			transform.parent.gameObject.GetComponent<PlayerHealth>().subtractPlayerHealth(5);
+			hurtPlayer(5);
 			// setHealthText ();
 		}
 	}
